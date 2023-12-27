79783a2086e1c6b6053cf47200d168dddfcce4ca
Rolly Maulana Awangga <awangga@gmail.com>
2023-12-27 08:43:29 +0700

    add  config

---
+++ base/jscroot/config.js
@@ -0,0 +1,7 @@
+export const croot = "https://jscroot.github.io/template/base/";
+
+export const url={
+    content : croot+"html/content.html",
+    header: croot+"html/header.html",
+    navbar:croot+"html/navbar.html"
+}
---
+++ base/jscroot/index.js
@@ -0,0 +1,7 @@
+import { insertHTML } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.3/croot.js";
+import { url } from "https://jscroot.github.io/template/base/jscroot/config.js";
+
+
+insertHTML(url.header,"header__container");
+insertHTML(url.navbar,"navbar");
+insertHTML(url.content,"content");
