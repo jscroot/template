763326d13bc5e4c850bef175ed1b7043fd0a2748
Rolly Maulana Awangga <awangga@gmail.com>
2023-12-27 20:02:48 +0700

    listen perubahan hash

--- base/jscroot/index.js
+++ base/jscroot/index.js
@@ -2,7 +2,6 @@ import { insertHTML } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.4/croot.
 import { url } from "https://jscroot.github.io/template/base/jscroot/config.js";
 import { showMenu,activeLink } from "https://jscroot.github.io/template/base/jscroot/controller/navbar.js";
 import { getContentURL } from "https://jscroot.github.io/template/base/jscroot/controller/content.js";
-import {getHash} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.2/croot.js";
 
 insertHTML(url.header,"header__container",runAfterHeader);
 
@@ -10,10 +9,8 @@ insertHTML(getContentURL(),"content",runAfterContent);
 
 
 window.addEventListener('hashchange', function(){
-    console.log("url hash berubah");
-    let hashlink=getHash();
-    console.log(hashlink);
-  }); 
+    insertHTML(getContentURL(),"content",runAfterContent);
+}); 
 
 function runAfterHeader(){
     insertHTML(url.navbar,"navbar",runAfterNavbar);
