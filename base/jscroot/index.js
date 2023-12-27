762b4d80093841e526e371f7818bad8bc702752f
Rolly Maulana Awangga <awangga@gmail.com>
2023-12-27 14:40:46 +0700

    runAfterHeader

--- base/jscroot/index.js
+++ base/jscroot/index.js
@@ -1,15 +1,20 @@
-import { insertHTML } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.3/croot.js";
+import { insertHTML } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.4/croot.js";
 import {runAfterAll} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.1/croot.js";
 import { url } from "https://jscroot.github.io/template/base/jscroot/config.js";
 import { showMenu,activeLink } from "https://jscroot.github.io/template/base/jscroot/controller/navbar.js";
 
 
 
-insertHTML(url.header,"header__container");
-insertHTML(url.navbar,"navbar");
+insertHTML(url.header,"header__container",runAfterHeader);
+
 insertHTML(url.content,"content");
 
-async function runMain(event){
+function runAfterHeader(){
+    insertHTML(url.navbar,"navbar",runAfterNavbar);
+    
+}
+
+function runAfterNavbar(){
     showMenu('header-toggle','navbar');
     activeLink('.nav__link');
 }
