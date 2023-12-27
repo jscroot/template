abeb9a22a0fab58b0509e95350c73846da927467
Rolly Maulana Awangga <awangga@gmail.com>
2023-12-27 09:38:13 +0700

    runafterdom

--- base/jscroot/config.js
+++ base/jscroot/config.js
@@ -1,7 +1,15 @@
 export const croot = "https://jscroot.github.io/template/base/";
 
+const folder={
+    html:"html/",
+    controller : "jscroot/controller/"
+}
+
+export const controllerfolder = croot+folder.controller;
+const templatefolder=croot+folder.html;
 export const url={
-    content : croot+"html/content.html",
-    header: croot+"html/header.html",
-    navbar:croot+"html/navbar.html"
+    content : templatefolder+"content.html",
+    header: templatefolder+"header.html",
+    navbar:templatefolder+"navbar.html",
+    controller : controllerfolder
 }
--- base/jscroot/index.js
+++ base/jscroot/index.js
@@ -1,7 +1,18 @@
 import { insertHTML } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.3/croot.js";
+import {runAfterDOM} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.1/croot.js";
 import { url } from "https://jscroot.github.io/template/base/jscroot/config.js";
+import { showMenu,activeLink } from "https://jscroot.github.io/template/base/jscroot/controller/navbar.js";
+
 
 
 insertHTML(url.header,"header__container");
 insertHTML(url.navbar,"navbar");
 insertHTML(url.content,"content");
+
+
+runAfterDOM(runAfter);
+
+function runAfter(){
+    showMenu('header-toggle','navbar');
+    activeLink('.nav__link');
+}
