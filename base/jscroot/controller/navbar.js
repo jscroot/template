efdd7f85cfd87d19ece09159c8a5e05326e0eb43
Rolly Maulana Awangga <awangga@gmail.com>
2023-12-27 15:06:06 +0700

    linkColor

--- base/jscroot/controller/navbar.js
+++ base/jscroot/controller/navbar.js
@@ -19,7 +19,7 @@ export function showMenu(headerToggle, navbarId){
 /*==================== LINK ACTIVE ====================*/
 export function activeLink(className){//'.nav__link'
     const linkColor = document.querySelectorAll(className);
-    linkColor.forEach(l => l.addEventListener('click', colorLink(linkColor)));
+    linkColor.forEach(l => {l.addEventListener('click', function() { colorLink(linkColor); } );});
 }
 function colorLink(linkColor){
     linkColor.forEach(l => l.classList.remove('active'));
