d45917695e1ce251898cc7481bd9b2af5f642dd5
Rolly Maulana Awangga <awangga@gmail.com>
2023-12-27 20:31:58 +0700

    refactoring

---
+++ base/jscroot/controller/main.js
@@ -0,0 +1,23 @@
+import { insertHTML } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.4/croot.js";
+import { url } from "https://jscroot.github.io/template/base/jscroot/config.js";
+import { showMenu,activeLink } from "https://jscroot.github.io/template/base/jscroot/controller/navbar.js";
+import { getContentURL } from "https://jscroot.github.io/template/base/jscroot/controller/content.js";
+
+
+export function runAfterHashChange(evt){
+    insertHTML(getContentURL(),"content",runAfterContent);
+}
+
+export function runAfterHeader(){
+    insertHTML(url.navbar,"navbar",runAfterNavbar);
+}
+
+export function runAfterNavbar(){
+    showMenu('header-toggle','navbar');
+    activeLink('.nav__link');
+    activeLink('.nav__dropdown-item');
+}
+
+export function runAfterContent(){
+    console.log("selesai get content");
+}
--- base/jscroot/index.js
+++ base/jscroot/index.js
@@ -1,30 +1,13 @@
 import { insertHTML } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.4/croot.js";
 import { url } from "https://jscroot.github.io/template/base/jscroot/config.js";
-import { showMenu,activeLink } from "https://jscroot.github.io/template/base/jscroot/controller/navbar.js";
 import { getContentURL } from "https://jscroot.github.io/template/base/jscroot/controller/content.js";
 import {onHashChange} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.4/croot.js";
+import {runAfterHeader,runAfterContent,runAfterHashChange} from "https://jscroot.github.io/template/base/jscroot/controller/main.js";
+
+
 
 insertHTML(url.header,"header__container",runAfterHeader);
-
 insertHTML(getContentURL(),"content",runAfterContent);
-
 onHashChange(runAfterHashChange);
 
 
-function runAfterHashChange(evt){
-    insertHTML(getContentURL(),"content",runAfterContent);
-}
-
-function runAfterHeader(){
-    insertHTML(url.navbar,"navbar",runAfterNavbar);
-}
-
-function runAfterNavbar(){
-    showMenu('header-toggle','navbar');
-    activeLink('.nav__link');
-    activeLink('.nav__dropdown-item');
-}
-
-function runAfterContent(){
-    console.log("selesai get content");
-}
