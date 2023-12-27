83fc575c0acc57f00d86e18f742d5c75528fe6df
Rolly Maulana Awangga <awangga@gmail.com>
2023-12-27 12:51:56 +0700

    sip

--- base/jscroot/index.js
+++ base/jscroot/index.js
@@ -1,22 +1,19 @@
 import { insertHTML } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.3/croot.js";
-import {runAfterDOM} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.1/croot.js";
+import {runAfterAll} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.1/croot.js";
 import { url } from "https://jscroot.github.io/template/base/jscroot/config.js";
 import { showMenu,activeLink } from "https://jscroot.github.io/template/base/jscroot/controller/navbar.js";
 
 
 
-async function loadComponent(){
-    insertHTML(url.header,"header__container");
-    insertHTML(url.navbar,"navbar");
-    insertHTML(url.content,"content");
-}
+insertHTML(url.header,"header__container");
+insertHTML(url.navbar,"navbar");
+insertHTML(url.content,"content");
 
-async function runMain(){
-    await loadComponent();
+async function runMain(event){
     showMenu('header-toggle','navbar');
     activeLink('.nav__link');
 }
 
-runMain();
+runAfterAll(runMain);
 
 
