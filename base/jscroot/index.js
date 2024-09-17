d0e27e407a2c3b5d0d2e5312b1a6c853cdb693b3
Rolly Maulana Awangga <awangga@gmail.com>
2024-09-17 13:39:31 +0700

    ok

--- base/jscroot/index.js
+++ base/jscroot/index.js
@@ -1,9 +1,9 @@
 import { insertHTML } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.4/croot.js";
 import {onHashChange} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.4/croot.js";
 // Please change https://jscroot.github.io/template/base/ into your base url
-import { url } from "../../template/base/jscroot/url/config.js";
-import { getContentURL } from "../../template/base/jscroot/url/content.js";
-import {runAfterHeader,runAfterContent,runAfterHashChange} from "../../template/base/jscroot/controller/main.js";
+import { url } from "../../url/config.js";
+import { getContentURL } from "../../url/content.js";
+import {runAfterHeader,runAfterContent,runAfterHashChange} from "../../controller/main.js";
 
 
 
